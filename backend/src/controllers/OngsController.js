9f844d5efe1a58e7ba3989e7f5154e7eb71b0c31
LucasCorrea <eslucascorrea@gmail.com>
2021-05-21 19:50:25 -0300

    Update to Use in New Computer

--- backend/src/controllers/OngsController.js
+++ backend/src/controllers/OngsController.js
@@ -6,14 +6,17 @@ module.exports = {
     async index (request, response) {
         const ongs = await connection('ongs').select('*');
         return response.json(ongs);
+       
     },
 
     
 
     async create(request, response) {
+        console.log(request.body);
+
         const { name, email, whatsapp, city, uf } = request.body;
         const id = crypto.randomBytes(4).toString('HEX');
-
+       
         await connection('ongs').insert({
             id,
             name,
